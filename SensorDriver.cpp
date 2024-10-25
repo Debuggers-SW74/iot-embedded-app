bc81933d7d43c9664ecc58d2dfbc729d8389b384
lecav30 <lecav30@outlook.com>
2024-10-25 15:35:07 -0500

    feat(component): bme280 sensor configuration added

--- SensorDriver.cpp
+++ SensorDriver.cpp
@@ -3,7 +3,15 @@
 #include "SensorDriver.h"
 
 SensorDriver::SensorDriver() {
-
+    // Configuración del Sensor BME280
+    bme.begin(0x76);
+    bme.setSampling(
+      Adafruit_BME280::MODE_FORCED,     // Modo de bajo consumo
+      Adafruit_BME280::SAMPLING_X1,     // Temperatura
+      Adafruit_BME280::SAMPLING_X1,     // Presión
+      Adafruit_BME280::SAMPLING_X1,     // Humedad
+      Adafruit_BME280::FILTER_OFF       // Sin filtrado
+    );
 }
 
 SensorState SensorDriver::collectData() {
