908d47a6c5b60f6fa3f72f07148683bef67288eb
Eduardo Chero <102326120+EChero11@users.noreply.github.com>
2024-10-25 18:16:11 -0500

    feat(operation): The schedule operations method was added for the senso

--- OperationManagement.cpp
+++ OperationManagement.cpp
@@ -7,13 +7,23 @@ OperationManagement::OperationManagement() {
 }
 
 void OperationManagement::executeOperationCycle() {
-
+  SensorState newState = sensorDriver->collectData();
+    
+  if (stateManager->updateSensorState(newState)) {
+    if (shouldSendData()) {
+      communicationManager->sendState();
+      lastOperationTime = millis();
+    }
+  }
 }
 
 void OperationManagement::scheduleOperations() {
-
+  unsigned long currentTime = millis();
+    
+  if (currentTime - lastOperationTime >= OPERATION_INTERVAL) {
+    executeOperationCycle();
+  }
 }
-
 bool OperationManagement::shouldSendData() {
 
 }
