3e04179d324db33cfe330a5b4a9e6ddfb1ae44eb
Claudio Moreno Rosales <u20191e800@upc.edu.pe>
2024-10-25 11:49:57 -0500

    feat(operation): added methods for operation management

--- OperationManagement.cpp
+++ OperationManagement.cpp
@@ -1,1 +1,19 @@
 /*OPERATION LAYER: Operation Management*/
+
+
+#include "OperationManagement.h"
+
+OperationManagement::OperationManagement() {
+}
+
+void OperationManagement::executeOperationCycle() {
+
+}
+
+void OperationManagement::scheduleOperations() {
+
+}
+
+bool OperationManagement::shouldSendData() {
+
+}
