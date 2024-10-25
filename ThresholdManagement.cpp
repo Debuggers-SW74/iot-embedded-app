99fc4a41fb84d1df9a30222f8c92d9890d5975d6
Claudio Moreno Rosales <u20191e800@upc.edu.pe>
2024-10-25 11:41:14 -0500

    feat(configuration): added methods for threshold management

--- ThresholdManagement.cpp
+++ ThresholdManagement.cpp
@@ -1,1 +1,28 @@
 /*CONFIGURATION LAYER: Threshold Management*/
+
+#include "ThresholdManagement.h"
+
+ThresholdManagement::ThresholdManagement(StateManagement* stateManager)
+  : stateManager(stateManager) {
+
+}
+
+bool ThresholdManagement::updateThresholds(const ThresholdState& thresholds) {
+
+}
+
+bool ThresholdManagement::updateTemperatureThreshold(float max) {
+
+}
+
+bool ThresholdManagement::updateHumidityThreshold(float max) {
+
+}
+
+bool ThresholdManagement::updatePressureThreshold(float max) {
+
+}
+
+const ThresholdState& ThresholdManagement::getThresholds() const {
+
+}
--- ThresholdManagement.h
+++ ThresholdManagement.h
@@ -1,1 +1,20 @@
 /*CONFIGURATION LAYER: Threshold Management*/
+
+#ifndef THRESHOLD_MANAGEMENT_H
+#define THRESHOLD_MANAGEMENT_H
+
+#include "StateManagement.h"
+
+class ThresholdManagement {
+private:
+    StateManagement* stateManager; 
+
+public:
+    explicit ThresholdManagement(StateManagement* stateManager);
+
+
+    // Threshold Queries
+    const ThresholdState& getThresholds() const;
+};
+
+#endif
