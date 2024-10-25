41dc9d4757980bbc948a8213946381ef2462432d
Claudio Moreno Rosales <u20191e800@upc.edu.pe>
2024-10-25 11:27:03 -0500

    feat(interface): added methods for communication management

--- CommunicationManager.cpp
+++ CommunicationManager.cpp
@@ -1,1 +1,27 @@
 /*INTERFACE LAYER: Communication Manager*/
+
+#include "CommunicationManager.h"
+#include <ArduinoJson.h>
+
+CommunicationManager::CommunicationManager(StateManagement* stateManager) 
+    : stateManager(stateManager), connected(false) { }
+
+bool CommunicationManager::connectWiFi() {
+
+}
+
+void CommunicationManager::sendState() {
+
+}
+
+String CommunicationManager::healthStatusToString(HealthStatus status) {
+
+}
+
+void CommunicationManager::sendHealth() {
+
+}
+
+bool CommunicationManager::publishEvent(const String& eventType, const String& sensorType) {
+	
+}
